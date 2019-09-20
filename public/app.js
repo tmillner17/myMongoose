290137036f8aaff125b7a5627e53c0cb67f12720
tyler <tmillner17@gmail.com>
2019-09-19 19:13:03 -0600

    homework progress

--- public/app.js
+++ public/app.js
@@ -13,5 +13,31 @@ $(document).on("click", "p", function(){
     })
     .then(function(data) {
         console.log(data);
+        $("#notes").append("<h2>" + data.title + "</h2>");
+        $("#notes").append("input id ='titleinput' name='title' >" );
+        $("#notes").append("textarea id='bodyinput' name='body'></textarea>");
+        $("#notes").append("<button data id='" + data._id + "'id='savenote'>Save Note</button>");
+        if(data.note) {
+            $("#titleinput").val(data.note.title);
+            $("#bodyinput").val(data.note.body);
+        }
+    });
+});
+
+$(document).on("click", "#savenote", function() {
+    let thisId = $(this).attr("data-id");
+    $.ajax({
+        method: 'POST',
+        url: "/articles/" + thisId,
+        data: {
+            title: $("titleinput").val(),
+            body: $("#bodyinput").val()
+        }
     })
-})
+    .then(function(data) {
+        console.log(data);
+        $("#notes").empty();
+    });
+$("#titleinput").val("");
+$("#titleinput").val("");
+});
